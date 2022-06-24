2a1b9a9cb705913fed337618bf6f552cac201f72
Florian Richter <florian.richter@sap.com>
2022-06-24 11:40:55 +0200

    Load github issues

--- hal9000/github.py
+++ hal9000/github.py
@@ -1,4 +1,5 @@
 import os
+import json
 from time import sleep
 from typing import Any
 import requests
@@ -8,14 +9,38 @@ BASE_URL = "https://api.github.com"
 
 
 def get(url: str) -> Any:
+    if url == "":
+        print("Called with empty URL. Something bad happened!")
+        return []
+
+    print("Getting issues...")
     res = requests.get(url, headers={"Authorization": f"token {token}"})
     if res.links.get("next"):
         if int(res.headers.get("X-RateLimit-Remaining", "0")) < 5:
             print("Rate limit is getting close...")
             sleep(5)
-        return res.json() + get(res.links.get("next", {}).get("url"))
+        return res.json() + get(res.links.get("next", {}).get("url", ""))
     return res.json()
 
 
 def get_issues(owner: str, repo: str):
-    return get(f"{BASE_URL}/repos/{owner}/{repo}/issues?state=all")
+    try:
+        with open("issues.json", "r") as issue_file:
+            issues = json.load(issue_file)
+            if len(issues):
+                print("Issues loaded from file")
+                print(len(issues))
+                # TODO load new items from API
+                return issues
+    except:
+        print("Error opening `issues.json`. File not present?")
+
+    issues = get(f"{BASE_URL}/repos/{owner}/{repo}/issues?state=all")
+    print("Issues loaded from API")
+
+    with open("issues.json", "w") as issue_file:
+        json.dump(issues, issue_file)
+        print("Issues saved to file")
+
+    print(len(issues))
+    return issues
--- hal9000/run.py
+++ hal9000/run.py
@@ -1,9 +1,14 @@
 # import spacy
-
+from pprint import pprint
 from hal9000.github import get_issues
 
 
 def main():
 
-    print(get_issues("SAP", "cloud-sdk-js"))
+    issues = get_issues("SAP", "cloud-sdk-js")
+
+    issue = issues[1234]
+
+    pprint(issue["title"])
+
     # nlp = spacy.load("en_core_web_trf")
